38fb8144d09fc86e7f1b3e9d2f4c7bc94363cf61
Amirhossein <dev.amirhbeigi@gmail.com>
2022-07-03 15:04:47 +0430

    ci: change base path in production build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: false,
   output: "standalone",
+  basePath: isProduction ? "/patient" : "",
 };
 
 module.exports = nextConfig;
