9774693c1e227053e577fb0b32f27727398a22d7
Amirhossein <dev.amirhbeigi@gmail.com>
2022-07-03 16:04:36 +0430

    fix: redirect url to auth

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,11 @@ import { NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const p24Session = request.cookies.get("P24SESSION");
   const certificate = request.cookies.get("certificate");
-  const siginPageRedirectUrl = new URL(`/signin/?url=${request.url}`, request.url);
+  const siginPageRedirectUrl = new URL(
+    `/signin/?url=https://www.paziresh24.com/patient/appointments`,
+    request.url
+  );
+  console.log(siginPageRedirectUrl);
   if (!certificate || !p24Session) {
     return NextResponse.redirect(siginPageRedirectUrl);
   }
