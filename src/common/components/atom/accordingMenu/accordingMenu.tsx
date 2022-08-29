4a2ffb8cc81f41acf0b5352a497a0a3712a50b41
AliRazavi <razavi.ali.1998@gmail.com>
2022-08-29 15:10:08 +0430

    feat: component according menu

---
+++ src/common/components/atom/accordingMenu/accordingMenu.tsx
@@ -0,0 +1,42 @@
+import ChevronIcon from '@/components/icons/chevron';
+import { useState } from 'react';
+import Text from '../text';
+
+interface AccordingMenuProps {
+  title: string;
+  items: Array<{
+    title: string;
+    link: string;
+  }>;
+}
+export const AccordingMenu: React.FC<AccordingMenuProps> = props => {
+  const { title, items } = props;
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="md:basis-[49%]">
+      <div className="bg-slate-50 cursor-pointer p-4 rounded-lg  " onClick={() => setOpen(!open)}>
+        <div className="flex justify-between items-center">
+          <Text as="h3" fontSize="sm" fontWeight="medium">
+            {title}
+          </Text>
+          <ChevronIcon dir={open ? 'top' : 'bottom'} />
+        </div>
+        {open ? (
+          <ul className="pr-1 text-sm  mt-2 select-none">
+            {items.map((link, index) => {
+              return (
+                <li key={index}>
+                  <a href={link.link} className="py-1 px-0 block text-xs">
+                    {link.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default AccordingMenu;
