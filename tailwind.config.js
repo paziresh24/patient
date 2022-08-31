2e01e81e2e0b16eef003f67b2f5b95cefdbb166f
Amirhossein <dev.amirhbeigi@gmail.com>
2022-08-31 07:32:29 +0430

    feat: add iransansX => variable fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: [''],
+      },
       colors: {
         primary: '#3861fb',
         gray: '#F8FAFB',
