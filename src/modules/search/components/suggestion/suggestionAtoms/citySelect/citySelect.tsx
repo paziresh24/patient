4c5a6f8f4cab4f3444456ab9b306752bd37e79ae
Amirhossein <dev.amirhbeigi@gmail.com>
2022-09-07 13:22:21 +0430

    fix: type safe

--- src/modules/search/components/suggestion/suggestionAtoms/citySelect/citySelect.tsx
+++ src/modules/search/components/suggestion/suggestionAtoms/citySelect/citySelect.tsx
@@ -61,6 +61,7 @@ export const CitySelect = (props: CitySelectProps) => {
       ...citiesData.current.find(item => item.id === cityId),
       id: cityId,
       name: citiesData.current.find(item => item.id === cityId)?.name ?? 'همه ایران',
+      en_slug: citiesData.current.find(item => item.id === cityId)?.en_slug ?? '',
     });
     setIsOpen(false);
   };
