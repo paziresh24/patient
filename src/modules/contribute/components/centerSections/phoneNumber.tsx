062197597adb1f3d6a90a0965cc51bf8ce58da42
AliRazavi <razavi.ali.1998@gmail.com>
2022-09-12 13:48:15 +0430

    chore: remove console .log

--- src/modules/contribute/components/centerSections/phoneNumber.tsx
+++ src/modules/contribute/components/centerSections/phoneNumber.tsx
@@ -24,7 +24,6 @@ export const PhoneNumberSection = (props: PhoneNumberSectionProps) => {
     const removedFieldWithIndex = phoneNumbers.filter((_, i) => i !== index);
     setPhoneNumbers(removedFieldWithIndex);
   };
-  console.log(phoneNumbers);
 
   return (
     <>
