47ab5f7e49cf9a2f762b35255b943d420d49ce56
amirhosseinmousavi <a.hossainmousavi@gmail.com>
2022-11-23 12:29:44 +0330

    chore: remove footer element for improve performance

--- src/common/components/layouts/footer/data/link.ts
+++ src/common/components/layouts/footer/data/link.ts
@@ -18,10 +18,6 @@ export const footerForUsers = {
       link: '/app',
     },
     {
-      title: 'اپلیکیشن مشاوره پذیرش ۲۴',
-      link: '/appconsult',
-    },
-    {
       title: 'جستجوی پزشکان و مراکز درمانی',
       link: '/s/',
     },
@@ -36,10 +32,6 @@ export const footerForUsers = {
     {
       title: 'سوالات متداول',
       link: '/home/faq/',
-    },
-    {
-      title: 'دکتر آنلاین',
-      link: '/consult',
     },
   ],
 };
@@ -70,44 +62,6 @@ export const footerPaziresh24 = {
     {
       title: 'قوانین',
       link: '/home/rules/',
-    },
-    {
-      title: 'آخرین اخبار',
-      link: '/blog/category/news/',
-    },
-  ],
-};
-
-export const footerForDoctors = {
-  title: 'برای پزشکان',
-  links: [
-    {
-      title: 'ورود پزشکان (مطب)',
-      link: 'https://dr.paziresh24.com/auth',
-    },
-    {
-      title: 'ورود پزشکان (مشاوره)',
-      link: '/doctor-login?url=/panel/doctor/#consult',
-    },
-    {
-      title: 'عضویت پزشکان',
-      link: '/home/fordoctors/',
-    },
-    {
-      title: 'عضویت رایگان نسخه نویسی',
-      link: '/home/%D9%86%D8%B3%D8%AE%D9%87-%D8%A7%D9%84%DA%A9%D8%AA%D8%B1%D9%88%D9%86%DB%8C%DA%A9/',
-    },
-    {
-      title: 'امکانات پذیرش24',
-      link: '/blog/category/%D8%A7%D9%85%DA%A9%D8%A7%D9%86%D8%A7%D8%AA-%D9%BE%D8%B0%DB%8C%D8%B1%D8%B4-24/',
-    },
-    {
-      title: 'دانلود اپلیکیشن پزشکان',
-      link: '/home/%D9%86%D8%B3%D8%AE%D9%87-%D9%88%DB%8C%DA%98%D9%87-%D9%BE%D8%B2%D8%B4%DA%A9%D8%A7%D9%86-%D8%AF%D8%A7%D9%86%D9%84%D9%88%D8%AF-%D8%A7%D9%BE%D9%84%DB%8C%DA%A9%DB%8C%D8%B4%D9%86/',
-    },
-    {
-      title: 'عضویت آنلاین مشاوره',
-      link: '/home/for-doctors-consult/',
     },
   ],
 };
