0fd92f31e98ee4c89201a5c0b2083bba124125dc
Amirhossein <dev.amirhbeigi@gmail.com>
2022-11-26 11:25:36 +0330

    chore: revert search input focus

--- src/modules/search/components/suggestion/searchBar.tsx
+++ src/modules/search/components/suggestion/searchBar.tsx
@@ -2,34 +2,25 @@ import Divider from '@/common/components/atom/divider';
 import clsx from 'clsx';
 import useTranslation from 'next-translate/useTranslation';
 import dynamic from 'next/dynamic';
-import { useEffect, useRef } from 'react';
 import { useSearchStore } from '../../store/search';
 import { SearchInput, SearchInputProps } from './suggestionAtoms/searchInput';
 const CitySelect = dynamic(() => import('./suggestionAtoms/citySelect'));
 
-interface SearchBarProps extends Omit<SearchInputProps, 'className' | 'autoFocus'> {
+interface SearchBarProps extends Omit<SearchInputProps, 'className'> {
   isOpenSuggestion?: boolean;
   onClickSearchInput?: () => void;
   onClickBackButton?: () => void;
   onEnter?: (text: string) => void;
   className?: string | object;
   onChangeCity: (value: any) => void;
-  autoFocus?: boolean;
 }
 
 export const SearchBar = (props: SearchBarProps) => {
   const { t } = useTranslation('search');
-  const { isOpenSuggestion, onClickSearchInput, onClickBackButton, onEnter, className, onChangeCity, autoFocus, ...rest } = props;
+  const { isOpenSuggestion, onClickSearchInput, onClickBackButton, onEnter, className, onChangeCity, ...rest } = props;
   const city = useSearchStore(state => state.city);
   const userSearchValue = useSearchStore(state => state.userSearchValue);
   const setUserSearchValue = useSearchStore(state => state.setUserSearchValue);
-  const searchInputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    if (autoFocus) {
-      setTimeout(() => searchInputRef.current?.focus(), 50);
-    }
-  }, [autoFocus]);
 
   return (
     <div
@@ -48,7 +39,6 @@ export const SearchBar = (props: SearchBarProps) => {
         clickBackButton={onClickBackButton}
         clikSearchButton={() => onEnter && onEnter(userSearchValue)}
         onKeyPress={e => e.key === 'Enter' && onEnter && onEnter(e.currentTarget?.value)}
-        ref={searchInputRef}
         {...rest}
       />
       <Divider orientation="vertical" height="2rem" />
