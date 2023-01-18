2aea73985fc70912dc1e3b767ba5d348b7ed6eb8
Amirhossein <dev.amirhbeigi@gmail.com>
2023-01-18 10:22:26 +0330

    fix: dont show divider when not support filter type

--- src/modules/search/components/filters/advancedSearch/index.tsx
+++ src/modules/search/components/filters/advancedSearch/index.tsx
@@ -10,13 +10,13 @@ export const AdvancedSearch = ({ className }: { className?: string }) => {
   if (filters.length === 0) return null;
   return (
     <div className={clsx('flex-col p-5 space-y-3 bg-white rounded-lg shadow-card', className)}>
-      {filters.map(
-        (item, index) =>
-          item.type !== 'slider_with_count' && (
-            <div key={item.title} className="flex flex-col space-y-3">
-              {item.type === 'radio' && <RadioFilter title={item.title} items={item.items} name={item.name} />}
-              {item.type === 'switch' && <SwitchFilter title={item.title} name={item.name} />}
-              {/* {item.type === 'slider_with_count' && (
+      {filters
+        .filter(item => ['radio', 'switch'].includes(item.type))
+        .map((item, index, filtedArray) => (
+          <div key={item.title} className="flex flex-col space-y-3">
+            {item.type === 'radio' && <RadioFilter title={item.title} items={item.items} name={item.name} />}
+            {item.type === 'switch' && <SwitchFilter title={item.title} name={item.name} />}
+            {/* {item.type === 'slider_with_count' && (
             <SliderFilter
               title={item.title}
               data={item.items.map((item: any) => ({ lable: item.title, count: item.count, value: item.value }))}
@@ -40,10 +40,9 @@ export const AdvancedSearch = ({ className }: { className?: string }) => {
               }
             />
           )} */}
-              {index + 1 !== filters.length && <Divider />}
-            </div>
-          ),
-      )}
+            {index + 1 !== filtedArray.length && <Divider />}
+          </div>
+        ))}
     </div>
   );
 };
