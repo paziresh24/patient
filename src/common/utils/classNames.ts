053860a55caef999c0b0c8a6d252815f69cb2a23
Amirhossein <dev.amirhbeigi@gmail.com>
2023-03-01 06:19:25 +0330

    refactor: add type for classNames util

--- src/common/utils/classNames.ts
+++ src/common/utils/classNames.ts
@@ -1,5 +1,6 @@
-import clsx from 'clsx';
+import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-export default function classNames(...classes: unknown[]) {
+
+export default function classNames(...classes: ClassValue[]) {
   return twMerge(clsx(classes));
 }
