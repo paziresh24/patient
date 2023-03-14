964b1ae0a7f33917eb5309cd596e1a26ead2a80e
Amirhossein <dev.amirhbeigi@gmail.com>
2023-03-14 12:56:59 +0330

    fix: api document change link

--- src/common/components/layouts/footer/data/link.ts
+++ src/common/components/layouts/footer/data/link.ts
@@ -49,7 +49,7 @@ export const footerPaziresh24 = {
     },
     {
       title: 'مستندات API',
-      link: 'https://apicurio-wss.paziresh24.info/sharing/3a0858f5-917f-4e09-8c88-11b77e053466/',
+      link: 'https://apicurio-wss.paziresh24.info/sharing/3a0858f5-917f-4e09-8c88-11b77e053466',
     },
     {
       title: 'فرصت‌های شغلی',
