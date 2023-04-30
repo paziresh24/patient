4ada6edf9f7be0c00acb5621f3ab21ee7cbb61f7
amirhosseinmousavi <a.hossainmousavi@gmail.com>
2023-04-30 14:26:37 +0430

    fix: solved uniq doctor messenger

--- src/modules/booking/functions/uniqMessengers.ts
+++ src/modules/booking/functions/uniqMessengers.ts
@@ -1,6 +1,5 @@
 import intersection from 'lodash/intersection';
-import uniq from 'lodash/uniq';
 
 export const uniqMessengers = (messengers: string[], uniqBy: string[]) => {
-  return uniq(intersection(messengers, uniqBy));
+  return intersection(messengers, uniqBy);
 };
