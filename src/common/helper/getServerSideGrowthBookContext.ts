6950add92b5dbbba32b32b044614036e7d630ee2
Amirhossein <dev.amirhbeigi@gmail.com>
2023-08-07 21:41:03 +0330

    fix: remove global this

--- src/common/helper/getServerSideGrowthBookContext.ts
+++ src/common/helper/getServerSideGrowthBookContext.ts
@@ -4,9 +4,9 @@ import { NextApiRequest } from 'next';
 export function getServerSideGrowthBookContext(req: NextApiRequest) {
   // Set GrowthBook polyfills for server environments
   setPolyfills({
-    fetch: globalThis.fetch || require('node:fetch'),
-    EventSource: globalThis.EventSource || require('eventsource'),
-    SubtleCrypto: globalThis.crypto?.subtle || require('node:crypto')?.webcrypto?.subtle,
+    fetch: require('node:fetch'),
+    EventSource: require('eventsource'),
+    SubtleCrypto: require('node:crypto')?.webcrypto?.subtle,
   });
 
   return {
