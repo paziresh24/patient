59dfc80f0f511ad03689adc027ad99fe0ab3f670
Amirhossein <dev.amirhbeigi@gmail.com>
2023-08-22 14:14:46 +0330

    fix: syntax error in section file

--- src/common/components/atom/section/section.tsx
+++ src/common/components/atom/section/section.tsx
@@ -15,7 +15,7 @@ export const Section: FunctionComponent<Props> = props => {
   const { id, children, title, ActionButton, className, dataAttributes } = props;
 
   return (
-    <section {...{ id }} className={classNames('flex flex-col w-full gap-y-3', className)} {...dataAttributes>
+    <section {...{ id }} className={classNames('flex flex-col w-full gap-y-3', className)} {...dataAttributes}>
       {(title || ActionButton) && (
         <div className="flex items-center justify-between px-4 md:px-0">
           <Text as="h2" fontWeight="bold">
