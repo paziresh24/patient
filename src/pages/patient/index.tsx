4239ab769124ffd455becd32c7728de7fd15e244
Amirhossein <dev.amirhbeigi@gmail.com>
2023-09-14 21:43:25 +0330

    fix: create component for login icon

--- src/pages/patient/index.tsx
+++ src/pages/patient/index.tsx
@@ -7,6 +7,7 @@ import CalenderIcon from '@/common/components/icons/calender';
 import DoctorIcon from '@/common/components/icons/doctor';
 import EditIcon from '@/common/components/icons/edit';
 import HeadphoneIcon from '@/common/components/icons/headphone';
+import LoginIcon from '@/common/components/icons/login';
 import LogoutIcon from '@/common/components/icons/logout';
 import ReceiptIcon from '@/common/components/icons/receipt';
 import SecuritySafeIcon from '@/common/components/icons/securitySafe';
@@ -112,23 +113,7 @@ export const PatinetProfile = () => {
                 onClick={() => openLoginForm()}
                 className="flex items-center px-5 py-4 border-b space-s-2 whitespace-nowrap border-slate-100"
               >
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M8.90002 7.55999C9.21002 3.95999 11.06 2.48999 15.11 2.48999H15.24C19.71 2.48999 21.5 4.27999 21.5 8.74999V15.27C21.5 19.74 19.71 21.53 15.24 21.53H15.11C11.09 21.53 9.24002 20.08 8.91002 16.54"
-                    stroke="#292D32"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path d="M2 12H14.88" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                  <path
-                    d="M12.65 8.6499L16 11.9999L12.65 15.3499"
-                    stroke="#292D32"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+                <LoginIcon />
                 <Text fontWeight="medium" fontSize="sm">
                   ورورد به حساب کاربری
                 </Text>
