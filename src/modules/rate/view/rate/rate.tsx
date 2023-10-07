57820359046ae46106c43064dd378267c22ecc44
amirhosseinmousavi <a.hossainmousavi@gmail.com>
2023-10-07 06:14:35 +0330

    chore: create component for close comment section

--- src/modules/rate/view/rate/rate.tsx
+++ src/modules/rate/view/rate/rate.tsx
@@ -4,9 +4,8 @@ import Divider from '@/common/components/atom/divider/divider';
 import Skeleton from '@/common/components/atom/skeleton/skeleton';
 import Text from '@/common/components/atom/text/text';
 import TextField from '@/common/components/atom/textField/textField';
-import AlertIcon from '@/common/components/icons/alert';
-import LikeIcon from '@/common/components/icons/like';
 import classNames from '@/common/utils/classNames';
+import CloseRate from '@/modules/profile/views/rateReview/closeRate';
 import FeedbackCard from '../../components/feedbackCard/feedbackCard';
 import { RateProps } from '../../type/rate';
 
@@ -67,30 +66,7 @@ export const Rate = (props: RateProps) => {
                 </div>
               </div>
             )}
-            {message && (
-              <div className="h-auto w-full relative flex flex-col items-center">
-                <div className="h-full m-4 blur-[0.32rem]">
-                  <p className="text-green-700 text-sm font-bold flex gap-1 items-center">
-                    این پزشک را پیشنهاد داده اند
-                    <LikeIcon style={{ transform: 'rotateY(180deg)' }} className="mb-1" />
-                  </p>
-                  <p className="text-sm font-medium text-justify !leading-7 mt-5">
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه
-                    و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای
-                    کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم
-                    افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت
-                    می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی
-                    دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
-                  </p>
-                </div>
-                <div className=" text-center flex flex-col justify-center items-center h-full w-full absolute bottom-0 gap-3 bg-[#FFAD0D]/[0.25]">
-                  <AlertIcon className="[&>path]:fill-[#FFAD0D] w-10 h-10" />
-                  <Text fontWeight="medium" className="text-[#0F1D40] leading-8">
-                    {message}
-                  </Text>
-                </div>
-              </div>
-            )}
+            {message && <CloseRate message={message} />}
           </>
         )}
       </div>
