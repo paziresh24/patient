885f15464084afe971fd9606583bd8e1bd7e607a
Amirhossein <dev.amirhbeigi@gmail.com>
2023-10-11 17:09:58 +0330

    fix: send correct gender to book api

--- src/common/utils/convertNumberToStringGender.ts
+++ src/common/utils/convertNumberToStringGender.ts
@@ -1,1 +1,1 @@
-export const convertNumberToStringGender = (number: string) => (number ? 'male' : 'female');
+export const convertNumberToStringGender = (number: string) => (+number === 1 ? 'male' : 'female');
