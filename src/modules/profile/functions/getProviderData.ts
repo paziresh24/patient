04786a5e68ef441ef6255b30c819d524bbe9ebcc
Amirhossein <dev.amirhbeigi@gmail.com>
2023-10-12 11:12:34 +0330

    fix: user_id is undefined

--- src/modules/profile/functions/getProviderData.ts
+++ src/modules/profile/functions/getProviderData.ts
@@ -8,7 +8,7 @@ export const getProviderData = async ({ slug }: GetProviderData) => {
   const firstItemData = response.providers?.[0] as Record<string, string>;
 
   if (!firstItemData?.user_id || !firstItemData?.employee_id) {
-    Sentry.captureMessage(`user_id:${firstItemData.user_id} employee_id:${firstItemData.employee_id}`);
+    Sentry.captureMessage(`user_id:${firstItemData?.user_id} employee_id:${firstItemData?.employee_id}`);
   }
 
   return {
