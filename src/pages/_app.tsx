5ffc602ea0b4df4e4f4885aa8b1b49528de3b1b8
Amirhossein <dev.amirhbeigi@gmail.com>
2023-10-22 13:36:37 +0330

    fix: recommed section loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,7 +29,7 @@ const iransansFont = localFont({
 const { publicRuntimeConfig } = getConfig();
 
 const isEnabledGrowthbook = !!publicRuntimeConfig.GROWTHBOOK_API_HOST && !!publicRuntimeConfig.GROWTHBOOK_CLIENT_KEY;
-const growthbook = new GrowthBook({
+export const growthbook = new GrowthBook({
   enabled: isEnabledGrowthbook,
   apiHost: publicRuntimeConfig.GROWTHBOOK_API_HOST,
   clientKey: publicRuntimeConfig.GROWTHBOOK_CLIENT_KEY,
