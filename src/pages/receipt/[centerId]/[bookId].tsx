f081e278c46bba95e3718d74e702eefc48ba1767
amirshahriarmousavi <a.hosainmousavi@gmail.com>
2023-11-15 15:42:04 +0330

    fix: restore deleted code

--- src/pages/receipt/[centerId]/[bookId].tsx
+++ src/pages/receipt/[centerId]/[bookId].tsx
@@ -1,10 +1,12 @@
 import { useGetReceiptDetails } from '@/common/apis/services/booking/getReceiptDetails';
 import { useGetServerTime } from '@/common/apis/services/general/getServerTime';
-import Alert from '@/common/components/atom/alert/alert';
+import Alert from '@/common/components/atom/alert';
 import Button from '@/common/components/atom/button';
+import Divider from '@/common/components/atom/divider';
 import Modal from '@/common/components/atom/modal/modal';
 import Skeleton from '@/common/components/atom/skeleton/skeleton';
 import Text from '@/common/components/atom/text';
+import TextField from '@/common/components/atom/textField';
 import ErrorIcon from '@/common/components/icons/error';
 import SuccessIcon from '@/common/components/icons/success';
 import TrashIcon from '@/common/components/icons/trash';
@@ -12,6 +14,7 @@ import { LayoutWithHeaderAndFooter } from '@/common/components/layouts/layoutWit
 import Seo from '@/common/components/layouts/seo';
 import { ClinicStatus } from '@/common/constants/status/clinicStatus';
 import { withCSR } from '@/common/hoc/withCsr';
+import { withServerUtils } from '@/common/hoc/withServerUtils';
 import useModal from '@/common/hooks/useModal';
 import usePdfGenerator from '@/common/hooks/usePdfGenerator';
 import usePwa from '@/common/hooks/usePwa';
@@ -60,6 +63,8 @@ const Receipt = () => {
     handleClose: handleCloseWaitingTimeModal,
     modalProps: waitingTimeModalProps,
   } = useModal();
+  const { handleOpen: handleOpenWaitingTimeFollowUpModal, modalProps: waitingTimeFollowUpModalProps } = useModal();
+  const [isWattingTimeFollowUpLoadingButton, setIsWattingTimeFollowUpLoadingButton] = useState(false);
 
   const getReceiptDetails = useGetReceiptDetails({
     book_id: bookId as string,
@@ -103,7 +108,7 @@ const Receipt = () => {
       if (getReceiptDetails.data.data?.data?.center?.waiting_time === 'بیشتر از یک ساعت' && !shuoldShowRateAppModal) {
         handleOpenWaitingTimeModal();
       }
-      if (!isPWA() && isActiveTurn && shuoldShowRateAppModal) {
+      if (isPWA() && isActiveTurn && shuoldShowRateAppModal) {
         handleOpenRateAppModal();
       }
     }
@@ -329,6 +334,37 @@ const Receipt = () => {
                   {turnStatus.visitedTurn ? 'استرداد وجه' : 'لغو نوبت'}
                 </Button>
               )}
+              {!!bookDetailsData && !turnStatus.deletedTurn && possibilityBeingVisited && (
+                <>
+                  <Divider />
+                  <div className="flex flex-col space-y-2">
+                    <Button
+                      size="sm"
+                      className="border-orange-300 text-orange-600 hover:bg-orange-50"
+                      block
+                      variant="secondary"
+                      onClick={handleOpenWaitingTimeFollowUpModal}
+                    >
+                      پیگیری تاخیر پزشک
+                    </Button>
+                    <Button
+                      size="sm"
+                      className="border-slate-300 text-slate-500 hover:bg-slate-50"
+                      block
+                      variant="secondary"
+                      onClick={() =>
+                        location.assign(
+                          `https://support.paziresh24.com/ticketbyturn/?book-id=${bookDetailsData.book_id}&pincode=${
+                            (pincode as string) ?? (user.id && md5(user.id))
+                          }`,
+                        )
+                      }
+                    >
+                      درخواست پشتیبانی این نوبت
+                    </Button>
+                  </div>
+                </>
+              )}
             </div>
           )}
         </div>
@@ -376,6 +412,27 @@ const Receipt = () => {
               مشاهده رسید نوبت
             </Button>
           </div>
+        </Modal>
+        <Modal {...waitingTimeFollowUpModalProps} title="پیگیری تاخیر پزشک">
+          <form
+            method="post"
+            onSubmit={() => setIsWattingTimeFollowUpLoadingButton(true)}
+            action="https://n8n.paziresh24.com/webhook/doctordelayfollowup"
+            className="flex flex-col space-y-3"
+          >
+            <Alert severity="warning" className="p-2">
+              <Text fontWeight="medium" fontSize="sm" className="text-orange-700">
+                با ارسال این فرم شما تایید می کنید که در زمان مقرر در پیامرسان انتخاب شده به پزشک پیام ارسال کرده اید و هنوز پاسخی دریافت
+                نکرده اید.
+              </Text>
+            </Alert>
+            <input name="book-id" value={bookDetailsData.book_id} type="hidden" />
+            <input name="pincode" value={(pincode as string) ?? (user.id && md5(user.id))} type="hidden" />
+            <TextField name="description" label="توضیحات" multiLine className="h-28" />
+            <Button type="submit" loading={isWattingTimeFollowUpLoadingButton}>
+              ارسال درخواست پیگیری
+            </Button>
+          </form>
         </Modal>
         <Modal title={rateAppModalInfo?.modal_title} {...rateAppModal}>
           <div className="flex flex-col space-y-3 items-center">
@@ -435,12 +492,14 @@ Receipt.getLayout = function getLayout(page: ReactElement) {
   );
 };
 
-export const getServerSideProps = withCSR(async (context: GetServerSidePropsContext) => {
-  return {
-    props: {
-      query: context.query,
-    },
-  };
-});
+export const getServerSideProps = withCSR(
+  withServerUtils(async (context: GetServerSidePropsContext) => {
+    return {
+      props: {
+        query: context.query,
+      },
+    };
+  }),
+);
 
 export default Receipt;
