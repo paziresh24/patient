11f43da6b789d66d8eb59d021b2a1e9abfb853cc
Amirhossein <dev.amirhbeigi@gmail.com>
2024-02-01 20:03:34 +0330

    feat: add .plasmic and plasmic.lock to source control

---
+++ .plasmic/plasmic/paziresh_24/icons/PlasmicIcon__Icon18.jsx
@@ -0,0 +1,38 @@
+// @ts-nocheck
+/* eslint-disable */
+/* tslint:disable */
+/* prettier-ignore-start */
+import React from "react";
+import { classNames } from "@plasmicapp/react-web";
+
+export function Icon18Icon(props) {
+  const { className, style, title, ...restProps } = props;
+  return (
+    <svg
+      xmlns={"http://www.w3.org/2000/svg"}
+      fill={"none"}
+      stroke={"currentColor"}
+      strokeWidth={"2"}
+      strokeLinecap={"round"}
+      strokeLinejoin={"round"}
+      className={classNames(
+        "plasmic-default__svg",
+        className,
+        "lucide lucide-shield-check"
+      )}
+      viewBox={"0 0 24 24"}
+      height={"1em"}
+      style={style}
+      {...restProps}
+    >
+      {title && <title>{title}</title>}
+
+      <path d={"M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10"}></path>
+
+      <path d={"M9 12l2 2 4-4"}></path>
+    </svg>
+  );
+}
+
+export default Icon18Icon;
+/* prettier-ignore-end */
