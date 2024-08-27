7660a3ef9b7d8a94bddb862e3200698bec7ed324
TahaZareian <166141372+TahaZareian@users.noreply.github.com>
2024-08-27 15:48:52 +0330

    change empty provider event splunk instance

--- src/modules/profile/functions/getProviderData.ts
+++ src/modules/profile/functions/getProviderData.ts
@@ -7,7 +7,7 @@ export const getProviderData = async ({ slug }: GetProviderData) => {
   const providerData: Record<string, string> = await providers({ slug });
 
   if (!providerData?.user_id) {
-    splunkInstance('doctor-profile').sendEvent({
+    splunkInstance('rokhnama').sendEvent({
       group: 'empty-providers',
       type: 'empty-providers-profile',
       event: {
