989e832a1c04d83607a352cc235dbf4435e646c8
hrg <hamidrezaghaseme@gmail.com>
2024-12-29 14:07:37 +0330

    feat: Add photo provider for Plasmic

--- src/common/fragment/components/gallery.tsx
+++ src/common/fragment/components/gallery.tsx
@@ -14,27 +14,31 @@ interface GalleryProps {
 }
 
 export const Gallery = (props: GalleryProps) => {
-  const { items, className, imageClassName, imageWidth = 120, imageHeight = 120 } = props;
+  const {
+    items = ['https://www.paziresh24.com/_next/static/media/logo.5e03fe79.svg'],
+    className,
+    imageClassName,
+    imageWidth = 120,
+    imageHeight = 120,
+  } = props;
 
   return (
-    <div className={classNames('p-4 pb-3', className)}>
-      <PhotoProvider>
-        <div className="flex pb-1 overflow-auto rounded-lg space-s-3">
-          {items.map((item, index) => (
-            <PhotoView key={index} src={item + '?size=100'}>
-              <Image
-                key={item}
-                src={item}
-                width={imageWidth}
-                height={imageHeight}
-                alt=""
-                className={classNames('rounded-md cursor-pointer max-h-40', imageClassName)}
-              />
-            </PhotoView>
-          ))}
-        </div>
-      </PhotoProvider>
-    </div>
+    <PhotoProvider>
+      <div className={classNames('flex pb-1 overflow-auto gap-3', className)}>
+        {items.map((item, index) => (
+          <PhotoView key={index} src={item + '?size=100'}>
+            <Image
+              key={item}
+              src={item}
+              width={imageWidth}
+              height={imageHeight}
+              alt=""
+              className={classNames('rounded-md cursor-pointer', imageClassName)}
+            />
+          </PhotoView>
+        ))}
+      </div>
+    </PhotoProvider>
   );
 };
 
@@ -46,6 +50,7 @@ export const galleryMeta: CodeComponentMeta<GalleryProps> = {
   props: {
     items: {
       type: 'array',
+      defaultValue: ['https://www.paziresh24.com/_next/static/media/logo.5e03fe79.svg'],
     },
     className: 'string',
     imageClassName: 'string',
