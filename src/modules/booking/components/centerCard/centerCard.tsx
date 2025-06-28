8d7c104a5d083d81938aadd63e158700c73cefe4
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-06-28 11:08:16 +0330

    feat: change center card styles

--- src/modules/booking/components/centerCard/centerCard.tsx
+++ src/modules/booking/components/centerCard/centerCard.tsx
@@ -25,56 +25,41 @@ export const CenterCard = (props: CenterCardProps) => {
           'cursor-default': isDisable || !isAvailable,
         })}
       >
-        <div className="p-4">
+        <div
+          className={classNames('p-3', {
+            "relative after:content-[''] opacity-65 after:absolute after:inset-0 after:pointer-events-none after:[background-image:repeating-linear-gradient(135deg,rgba(0,0,0,0.05)_0_2px,transparent_2px_7px)]":
+              isDisable,
+          })}
+        >
           <div className="flex items-center w-full">
-            <div
-              className={classNames('py-[0.5rem] rounded-md', {
-                'bg-[#3861FB]/[0.2]': type === 'office' && !isDisable && isAvailable,
-                'bg-[#FF620A]/[0.2]': type === 'hospital' && !isDisable && isAvailable,
-                'bg-[#27BDA0]/[0.2]': type === 'consult' && !isDisable && isAvailable,
-                'bg-slate-200 text-slate-500': isDisable || !isAvailable,
-              })}
-            >
-              {type === 'office' && <OfficeIcon className="w-10 h-6" />}
-              {type === 'hospital' && <HospitalIcon className="w-10 h-6" />}
-              {type === 'consult' && <PhoneIcon className="w-10 h-6" />}
-            </div>
-            <div
-              className={classNames(
-                'mr-2 flex justify-between items-center text-black w-full p-[0.65rem] rounded-md  text-sm font-medium',
-                {
-                  'bg-[#3861FB]/[0.2]': type === 'office' && !isDisable && isAvailable,
-                  'bg-[#FF620A]/[0.2]': type === 'hospital' && !isDisable && isAvailable,
-                  'bg-[#27BDA0]/[0.2]': type === 'consult' && !isDisable && isAvailable,
-                  'bg-slate-200 !text-slate-500': isDisable || !isAvailable,
-                },
-              )}
-            >
-              <Text className="line-clamp-1">{name}</Text>
+            <div className={classNames('flex justify-between items-center w-full text-sm')}>
+              <Text className={classNames('line-clamp-1 font-semibold', { 'text-primary': !isDisable })}>{name}</Text>
               <ChevronIcon dir="left" className="min-w-2" />
             </div>
           </div>
           {!!address && (
-            <Text fontSize="sm" className="block w-full mt-4 text-slate-400 line-clamp-1">
+            <Text fontSize="xs" className="w-full mt-1 text-slate-400 line-clamp-1">
               {address}
             </Text>
           )}
         </div>
         {(!!freeturn || !!waitingTimeInfo?.waiting_time_title) && !isDisable && isAvailable && (
-          <>
-            <Divider className="mb-4" />
+          <div className="pb-3">
+            <Divider className="mb-3" />
             {freeturn && (
-              <div className="flex justify-between w-full px-4 mb-2">
+              <div className="flex justify-between w-full px-3">
                 <Text className="text-black/">{type === 'consult' ? 'زمان پاسخگویی' : 'اولین نوبت خالی'}:</Text>
-                <Text className="block text-slate-500">{freeturn}</Text>
+                <Text className="block" fontWeight="semiBold">
+                  {freeturn}
+                </Text>
               </div>
             )}
             {!!waitingTimeInfo?.waiting_time_title && (
-              <Text fontSize="sm" className="block w-full px-4 pb-4 text-slate-600">
-                طبق نظر بیماران قبلی، میانگین زمان انتظار ویزیت: <strong>{waitingTimeInfo?.waiting_time_title}</strong>
+              <Text fontSize="xs" className="block w-full px-3 mt-1 text-slate-600">
+                میانگین زمان انتظار ویزیت: <strong>{waitingTimeInfo?.waiting_time_title}</strong>
               </Text>
             )}
-          </>
+          </div>
         )}
         {!isAvailable && availableTime && (
           <>
@@ -90,8 +75,8 @@ export const CenterCard = (props: CenterCardProps) => {
         {isDisable && (
           <>
             <Divider />
-            <div className="w-full p-4">
-              <Text fontSize="sm" align="center" fontWeight="medium" className="block text-slate-500">
+            <div className="w-full p-3">
+              <Text fontSize="sm" align="center" fontWeight="bold" className="block">
                 نوبت دهی اینترنتی در این {type === 'office' ? 'مطب' : 'مرکز'} غیر فعال است.
               </Text>
               <PhoneNumberList phoneNumbers={phoneNumbers} type={type} />
