0975544694bd33fe414da3fe3d5e68694bf97551
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-07-28 18:18:17 +0330

    feat: add growthbook feature for visits api endpoint

--- src/common/apis/services/booking/getBooks.ts
+++ src/common/apis/services/booking/getBooks.ts
@@ -1,6 +1,7 @@
 import { paziresh24AppClient } from '@/common/apis/client';
 import { ServerStateKeysEnum } from '@/common/apis/serverStateKeysEnum';
 import { useQuery } from '@tanstack/react-query';
+import { growthbook } from 'src/pages/_app';
 
 interface Params {
   return_type: 'book' | 'book_request' | 'online_visit';
@@ -9,7 +10,9 @@ interface Params {
 }
 
 export const getBooks = (params: Params) => {
-  return paziresh24AppClient.get('/V1/patient/visits', { params });
+  const endpoints = growthbook.getFeatureValue<Record<string, string>>('booking:api-endpoints', {});
+
+  return paziresh24AppClient.get(endpoints?.visits ?? '/V1/patient/visits', { params });
 };
 
 export const useGetBooks = (params: Params, options?: any) => {
