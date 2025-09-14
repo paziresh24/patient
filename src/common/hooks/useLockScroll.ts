48275bcb0cbca81bb8da0a18e7e4ea24b9fe4fc4
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-09-14 19:03:12 +0330

    fix: open lock scroll

--- src/common/hooks/useLockScroll.ts
+++ src/common/hooks/useLockScroll.ts
@@ -1,30 +1,14 @@
-import { useCallback, useRef } from 'react';
-
-let lockCount = 0; // شمارنده‌ی سراسری
+import { useCallback } from 'react';
 
 export const useLockScroll = () => {
-  const lockedRef = useRef(false);
-
   const lockScroll = useCallback(() => {
-    if (!lockedRef.current) {
-      lockedRef.current = true;
-      lockCount += 1;
-      if (lockCount === 1) {
-        document.body.classList.add('overflow-hidden');
-        document.body.classList.add('md:pr-[0.3rem]');
-      }
-    }
+    document.body.classList.add('overflow-hidden');
+    document.body.classList.add('md:pr-[0.3rem]');
   }, []);
 
   const openScroll = useCallback(() => {
-    if (lockedRef.current) {
-      lockedRef.current = false;
-      lockCount = Math.max(0, lockCount - 1);
-      if (lockCount === 0) {
-        document.body.classList.remove('overflow-hidden');
-        document.body.classList.remove('md:pr-[0.3rem]');
-      }
-    }
+    document.body.classList.remove('overflow-hidden');
+    document.body.classList.remove('md:pr-[0.3rem]');
   }, []);
 
   return { lockScroll, openScroll };
