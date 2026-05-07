1da0b4f6e435d8119453f13ca7a8aa707f7f9d08
hojatkhajavi <hojatkhajavi@gmail.com>
2026-05-07 15:28:51 +0000

    Edit getBalance.ts

--- src/common/apis/services/wallet/getBalance.ts
+++ src/common/apis/services/wallet/getBalance.ts
@@ -1,9 +1,9 @@
-import { katibeApiClient } from '@/common/apis/client';
+import axios from 'axios';
 import { ServerStateKeysEnum } from '@/common/apis/serverStateKeysEnum';
 import { useQuery } from '@tanstack/react-query';
 
 export const getBalance = async () => {
-  return katibeApiClient.get(`/v1/transactions/balance/p24`);
+  return axios.get('/patient/api/wallet/balance');
 };
 
 export const useGetBalance = (options?: any) => {
