2d99caf432e58fce93f61b816c7885d9a1b1eaa7
nirweb.ir <nirweb.ir@gmail.com>
2026-05-17 21:54:13 +0330

    add object-fit: cover for profile pic

--- src/common/utils/picUserImageUrl.ts
+++ src/common/utils/picUserImageUrl.ts
@@ -4,3 +4,6 @@ export const picUserImageUrl = (userId: string | number): string =>
   `${PIC_USER_IMAGE_BASE}/${userId}?p=thumb`;
 
 export const PIC_USER_IMAGE_FALLBACK_URL = picUserImageUrl(1);
+
+export const isPicUserImageUrl = (src?: string | null): src is string =>
+  typeof src === 'string' && src.includes(`${PIC_USER_IMAGE_BASE}/`);
